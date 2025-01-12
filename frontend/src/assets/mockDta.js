3356f4a29a895ea6c3cd163db9f0a5e85bbe2c75
niceman1234man <yihunietarekegn18@gmail.com>
2025-01-12 08:58:07 -0800

    packages

---
+++ frontend/src/assets/mockDta.js
@@ -0,0 +1,72 @@
+export const catagories = [
+  "Electronics",
+  "Fashion",
+  "Home&kitchen",
+  "Beauty",
+  "Sports",
+  "Automotive",
+];
+
+export const mockdata = [
+  {
+    id: 1,
+    image: "",
+    name: "product 1",
+    price: 9.99,
+  },
+  {
+    id: 2,
+    image: "",
+    name: "product 2",
+    price: 19.99,
+  },
+
+  {
+    id: 3,
+    image: "",
+    name: "product 3",
+    price: 29.99,
+  },
+  {
+    id: 4,
+    image: "",
+    name: "product 4",
+    price: 39.99,
+  },
+  {
+    id: 5,
+    image: "",
+    name: "product 5",
+    price: 39.99,
+  },
+  {
+    id: 6,
+    image: "",
+    name: "product 6",
+    price: 39.99,
+  },
+  {
+    id: 7,
+    image: "",
+    name: "product 7",
+    price: 39.99,
+  },
+  {
+    id: 8,
+    image: "",
+    name: "product 8",
+    price: 39.99,
+  },
+  {
+    id: 9,
+    image: "",
+    name: "product 9",
+    price: 39.99,
+  },
+  {
+    id: 10,
+    image: "",
+    name: "product 10",
+    price: 39.99,
+  },
+];
