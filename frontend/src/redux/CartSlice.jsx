1fd6f2cd74d3b66764bae7fd427a5852552e0ba2
niceman1234man <yihunietarekegn18@gmail.com>
2025-01-12 08:54:12 -0800

    cart slice

---
+++ frontend/src/redux/CartSlice.jsx
@@ -0,0 +1,72 @@
+import {createSlice } from '@reduxjs/toolkit'
+
+const initialState={
+    products:[],
+    totalQuantity:0,
+    totalPrice:0
+}
+
+const cartSlice=createSlice({
+    name:'carts',
+    initialState,
+    reducers:{
+       addToCart(state,action){
+       const newItem=action.payload
+        const itemIndex=state.products.find((item)=>item.id===newItem.id)
+        if(itemIndex){
+         itemIndex.quantity++;
+         itemIndex.totalPrice+=newItem.price;
+
+        }else{
+            state.products.push({
+                id:newItem.id,
+                name:newItem.name,
+                quantity:1,
+                image:newItem.image,
+                totalPrice:newItem.price
+
+            })
+            
+        }
+        state.totalPrice+=newItem.price;
+        state.totalQuantity++;
+       },
+   
+       removeFromCart(state,action){
+        const id=action.payload
+        const findItem=state.products.find((item)=>item.id===id)
+        if(findItem){
+            state.totalPrice-=findItem.totalPrice;
+            state.totalQuantity-=findItem.quantity;
+            state.products=state.products.filter((item)=>item.id!==id);
+        }
+       },
+
+       increaseQuantity(state,action){
+        const id=action.payload
+        const findItem=state.products.find((item)=>item.id===id)
+        if(findItem){
+            findItem.quantity++;
+            findItem.totalPrice+=findItem.price;
+            state.totalQuantity++;
+            state.totalPrice +=findItem.price;
+        }
+       },
+       decreaseQuantity(state,action){
+        const id=action.payload
+        const findItem=state.products.find((item)=>item.id===id)
+        if(findItem){
+            if(findItem.quantity>1){
+            findItem.quantity--;
+            findItem.totalPrice-=findItem.price;
+            state.totalQuantity--;
+            state.totalPrice -=findItem.price;
+        }
+        }
+    
+       }
+    }
+})
+
+export const {addToCart,removeFromCart,increaseQuantity,decreaseQuantity}= cartSlice.actions
+export default cartSlice.reducer
